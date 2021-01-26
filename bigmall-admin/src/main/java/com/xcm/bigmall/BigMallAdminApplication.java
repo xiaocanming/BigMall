a5659b4d4eba5b75329957f0f16d94634c5470bf
X <x@qq.com>
2021-01-26 17:48:43 +0800

    1.修改admin不能启动

--- bigmall-admin/src/main/java/com/xcm/bigmall/BigMallAdminApplication.java
+++ bigmall-admin/src/main/java/com/xcm/bigmall/BigMallAdminApplication.java
@@ -3,12 +3,15 @@ package com.xcm.bigmall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @描述 后台项目启动
  * @创建人 xcm
  * @创建时间 2021/1/7
  */
+//FeignClient实现类注解
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BigMallAdminApplication {
