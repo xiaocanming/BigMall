39a6eb25f17cd9306de59e5db7626cd9e7f2b305
xiaocanming <1339659169@qq.com>
2021-02-01 14:50:36 +0800

    1.添加admin监控

---
+++ bigmall-monitor/src/main/java/com/xcm/bigmall/BigMallMonitorApplication.java
@@ -0,0 +1,22 @@
+package com.xcm.bigmall;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @描述
+ * @创建人 xcm
+ * @创建时间 2021/2/1
+ */
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class BigMallMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigMallMonitorApplication.class, args);
+    }
+
+}
